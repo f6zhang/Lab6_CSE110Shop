2ee3818034520c59c878d8f785bd941c9265686e
f6zhang <47405797+f6zhang@users.noreply.github.com>
2021-02-11 03:06:13 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
+  
+  if (localStorage.getItem("count") != null){
+    let count = document.getElementById("cart-count");
+    count.textContent = localStorage.getItem("count");
+  }
+  
   fetch("https://fakestoreapi.com/products")
   .then(response => response.json())
   .then(data => {
@@ -8,7 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
     let product_list = document.getElementById("product-list");
     for (let i=0; i < data.length; i++){
       let product = document.createElement("product-item");
-      product.setAttribute('data', data[i]);
+      product.setAttribute('data', JSON.stringify(data[i]));
       product_list.appendChild(product);
     }
   })
