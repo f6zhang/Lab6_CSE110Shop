30c7520b7a1fc3524d1933628fd2f54abc450929
f6zhang <47405797+f6zhang@users.noreply.github.com>
2021-02-10 18:53:58 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 // Script.js
 
+let my_data;
+
 window.addEventListener('DOMContentLoaded', () => {
   fetch("https://fakestoreapi.com/products")
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => my_data = data);
 });
