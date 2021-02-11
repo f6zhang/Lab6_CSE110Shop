be49ea5f85f10285cfea7936f8623793d1141ee6
f6zhang <47405797+f6zhang@users.noreply.github.com>
2021-02-11 00:08:09 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
 // Script.js
 
-let my_data;
-
 window.addEventListener('DOMContentLoaded', () => {
   fetch("https://fakestoreapi.com/products")
   .then(response => response.json())
   .then(data => {
     localStorage.setItem("data", data);
+    let product_list = document.getelementbyid("product-list");
+    for (let i=0; i < data.length; i++){
+      let product = document.createElement("product-item", {data = data[i]});
+      product_list.appendChild(product);
+    }
+  })
 });
