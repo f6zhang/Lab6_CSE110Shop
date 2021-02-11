2798bc9dd1e96ae286a1ef93ae7b3788ed9ba452
f6zhang <47405797+f6zhang@users.noreply.github.com>
2021-02-11 01:38:09 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ window.addEventListener('DOMContentLoaded', () => {
   .then(response => response.json())
   .then(data => {
     localStorage.setItem("data", data);
-    let product_list = document.getelementbyid("product-list");
+    let product_list = document.getElementById("product-list");
     for (let i=0; i < data.length; i++){
       let product = document.createElement("product-item");
       product.setAttribute('data', data);
