8cddf6022e675244abb8391f9ea050e591338a3e
f6zhang <47405797+f6zhang@users.noreply.github.com>
2021-02-10 22:45:28 -0800

    Update product-item.js

--- components/product-item.js
+++ components/product-item.js
@@ -3,36 +3,116 @@
 class ProductItem extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({mode: 'open'});
+    let shadow = this.attachShadow({mode: 'open'});
     
     let wrapper = document.createElement('span');
     wrapper.setAttribute('class', 'wrapper');
     
     let img = document.createElement('img');
-    img.width = 200;
-    
+    img.width = 200;   
     if (this.hasAttribute('data')){
       img.src = data.image;
       img.alt = data.title;
     }
-    
     wrapper.appendChild(img);
     
     let title = document.createElement('span');
-    
     title.setAttribute('class', 'title');
     if (this.hasAttribute('data')){
       title.textContent = data.title;
     }
+    wrapper.appendChild(title);
     
+    let price = document.createElement('span');
+    
+    price.setAttribute('class', 'price');
+    if (this.hasAttribute('data')){
+      title.textContent = "$" + data.price;
+    }
+    wrapper.appendChild(price);
+    
+    let button = document.createElement("button");
+    button.onclick = function(){
+       alert('Added to Cart!');
+    }
+    button.textContent = "Add to Cart";
+    wrapper.appendChild(button);
+
+    const style = document.createElement('style');
+    
+    style.textContent = '
+      .price {
+        color: green;
+        font-size: 1.8em;
+        font-weight: bold;
+         margin: 0;
+      }
+
+      .product {
+        align-items: center;
+        background-color: white;
+        border-radius: 5px;
+        display: grid;
+        grid-template-areas: 
+        'image'
+        'title'
+        'price'
+        'add';
+        grid-template-rows: 67% 11% 11% 11%;
+        height: 450px;
+        filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+        margin: 0 30px 30px 0;
+        padding: 10px 20px;
+        width: 200px;
+      }
+
+      .product > button {
+        background-color: rgb(255, 208, 0);
+        border: none;
+        border-radius: 5px;
+        color: black;
+        justify-self: center;
+        max-height: 35px;
+        padding: 8px 20px;
+        transition: 0.1s ease all;
+      }
+
+      .product > button:hover {
+        background-color: rgb(255, 166, 0);
+        cursor: pointer;
+        transition: 0.1s ease all;
+      }
+
+      .product > img {
+        align-self: center;
+        justify-self: center;
+        width: 100%;
+      }
+
+      .title {
+        font-size: 1.1em;
+        margin: 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+
+      .title:hover {
+        font-size: 1.1em;
+        margin: 0;
+        white-space: wrap;
+        overflow: auto;
+        text-overflow: unset;
+      }
+
+    ';
+
+    // Attach the created elements to the shadow dom
+    shadow.appendChild(style);
+    shadow.appendChild(wrapper);
     
   }
-                <!-- li class="product">
-                    <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200>
-                    <p class="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops</p>
-                    <p class="price">$109.95</p>
-                    <button onclick="alert('Added to Cart!')">Add to Cart</button>
-                </li -->
+           
 }
 
 customElements.define('product-item', ProductItem);
